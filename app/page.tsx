77c90325969749bde0e0c7a457880cb95b23f504
maki <marijanadev@gmail.com>
2024-09-20 17:05:52 +0200

    Hero section has been added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 //import Clients from "@/components/Clients";
 //import Competitors from "@/components/Competitors";
 //import Footer from "@/components/Footer";
-//import Hero from "@/components/Hero";
+
 //import { InfoCards } from "@/components/InfoCard";
 //import Leaders from "@/components/Leaders";
 import { Navbar } from "@/components/Navbar";
+import Hero from "@/components/Hero";
 //import { StickyScrollRevealDemo } from "@/components/StickyScrollDemo";
 //import { VideoYT } from "@/components/VideoYT";
 
@@ -12,6 +13,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
       <h3>HELLO WORLD</h3>
     </main>
   );
@@ -20,7 +22,7 @@ export default function Home() {
 
       {/*
       
-      <Hero />
+      
       <InfoCards />
       <Competitors/>
       <Leaders/>
