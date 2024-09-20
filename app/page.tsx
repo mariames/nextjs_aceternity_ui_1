1a53aa20562e97c578dd3d9abf94c831a72e3736
maki <marijanadev@gmail.com>
2024-09-20 17:09:21 +0200

    Info cards have been added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import { Navbar } from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import { InfoCards } from "@/components/InfoCard";
 //import Clients from "@/components/Clients";
 //import Competitors from "@/components/Competitors";
 //import Footer from "@/components/Footer";
 
-//import { InfoCards } from "@/components/InfoCard";
+//
 //import Leaders from "@/components/Leaders";
-import { Navbar } from "@/components/Navbar";
-import Hero from "@/components/Hero";
+
 //import { StickyScrollRevealDemo } from "@/components/StickyScrollDemo";
 //import { VideoYT } from "@/components/VideoYT";
 
@@ -14,7 +16,7 @@ export default function Home() {
     <main>
       <Navbar />
       <Hero />
-      <h3>HELLO WORLD</h3>
+      <InfoCards />
     </main>
   );
 }
@@ -23,7 +25,7 @@ export default function Home() {
       {/*
       
       
-      <InfoCards />
+      
       <Competitors/>
       <Leaders/>
       <StickyScrollRevealDemo />
