cdc353abd5dccecf1beaa0d2847788be17b52fec
maki <marijanadev@gmail.com>
2024-09-20 17:03:11 +0200

    navbar has been added

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@
 //import Hero from "@/components/Hero";
 //import { InfoCards } from "@/components/InfoCard";
 //import Leaders from "@/components/Leaders";
-//import { Navbar } from "@/components/Navbar";
+import { Navbar } from "@/components/Navbar";
 //import { StickyScrollRevealDemo } from "@/components/StickyScrollDemo";
 //import { VideoYT } from "@/components/VideoYT";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <h3>HELLO WORLD</h3>
     </main>
   );
@@ -18,7 +19,7 @@ export default function Home() {
 
 
       {/*
-      <Navbar />
+      
       <Hero />
       <InfoCards />
       <Competitors/>
