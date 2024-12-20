2964d2d179c2a101f5cb1aee5b3d12f10188ea07
marijanadev <marijanavujovic92@gmail.com>
2024-12-20 22:27:03 +0100

    change in the description of the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kompyte",
-  description: "Kompyte is a competitive intelligence software focused on enabling go-to-market teams with the insights they need to overcome the competition. Using machine learning AI, Kompyte collects, analyzes, and organizes data to help teams generate and share actionable insights.",
+  description: "Kompyte is a competitive intelligence software focused on enabling go-to-market teams with the insights they need to overcome the competition. Using machine learning AI, Kompyte collects,  analyzes, and organizes data to help teams share actionable insights.",
 };
 
 export default function RootLayout({
